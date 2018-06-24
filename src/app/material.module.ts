5f3473c8b7df58c020e2003521302214a8354d3c
Pete Russell <pinkbatz@gmail.com>
2018-06-24 21:26:53 +1200

    Adds nav list styling and static implementation.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
     imports: [
-        MatSidenavModule
+        MatSidenavModule,
+        MatExpansionModule,
+        MatListModule
     ],
     exports: [
-        MatSidenavModule
+        MatSidenavModule,
+        MatExpansionModule,
+        MatListModule
     ]
 })
 export class MaterialModule {}
