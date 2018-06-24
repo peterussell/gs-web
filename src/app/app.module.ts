76b9ff1b7f0f3ff8673eb47b386f23554a513df6
Pete Russell <pinkbatz@gmail.com>
2018-06-24 16:12:34 +1200

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { PageTitleComponent } from './page-title/page-title.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PageTitleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@NgModule({
+    imports: [
+        MatSidenavModule
+    ],
+    exports: [
+        MatSidenavModule
+    ]
+})
+export class MaterialModule {}
