c6936a14bd9d8346097619f99929052ea1f79258
Pete Russell <pinkbatz@gmail.com>
2018-08-08 22:15:27 +1200

    First round of mock work (remove if working online).

--- src/app/core/models/question.model.ts
+++ src/app/core/models/question.model.ts
@@ -1,6 +1,9 @@
+import { Reference } from "./reference.model";
+
 export class Question {
     constructor(
         public questionId: string,
         public question: string,
-        public answer: string) {}
+        public answer: string,
+        public references: Array<Reference>) {}
 }
