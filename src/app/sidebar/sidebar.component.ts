b06db0d2230534b0c6c07df23d531038091bef38
Pete Russell <pinkbatz@gmail.com>
2019-09-19 14:48:52 +1200

    Sidenav makeover. Add register and sign in links to sidenav.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../core/services/api.service';
-import { Course } from '../core/models/course.model';
-import { Subject } from '../core/models/subject.model';
-import { QuestionService } from '../core/services/question.service';
+import { MatDialog } from '@angular/material';
+import { AccountDialogComponent, AccountDialogState } from '../account-dialog/account-dialog.component';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,33 +8,27 @@ import { QuestionService } from '../core/services/question.service';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  public courses: { [id: number]: Course };
 
-  constructor(private apiService: ApiService,
-    private questionService: QuestionService) { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
-    this.apiService.getCourses().subscribe(res => {
-      this.courses = this.sortCourses(res.Courses);
-    },
-    (error: any) => {
-      console.log(error.message);
+  }
+
+  registerClick() {
+    const dialogRef = this.dialog.open(AccountDialogComponent, {
+      width: '380px',
+      panelClass: 'container-no-padding',
+      position: { top: '30px' },
+      data: { initialState: AccountDialogState.Register }
     });
   }
 
-  sortCourses(courses: Array<Course>): Array<Course> {
-    return courses.sort((a, b) => {
-      if (a.Order > b.Order) return 1;
-      if (a.Order < b.Order) return -1;
-      return 0;
-    });
-  }
-
-  sortSubjects(subjects: Array<Subject>): Array<Subject> {
-    return subjects.sort((a, b) => {
-      if (a.Title > b.Title) return 1;
-      if (a.Title < b.Title) return -1;
-      return 0;
+  signInClick() {
+    const dialogRef = this.dialog.open(AccountDialogComponent, {
+      width: '380px',
+      panelClass: 'container-no-padding',
+      position: { top: '30px' },
+      data: { initialState: AccountDialogState.Login }
     });
   }
 }
